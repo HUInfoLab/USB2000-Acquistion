8f5c9ef33a1fffc63f69ac3727c6a337be43263c
tjm5114 <tjm5114@gmail.com>
2015-06-24 11:51:59 -0400

    Fire capture spectrum function on button press

--- USB2000Acquire.py
+++ USB2000Acquire.py
@@ -24,55 +24,18 @@ spec = sb.Spectrometer(devices[0])
 #open file to write data to
 f = open('spectralCapture.txt', 'w')
 
-#While True:
-	#intTime = input("What is your integration time")
+#define capture function to be called on button press
+def capture_spectrum():
+	wavelengthArray = spec.wavelengths()
+	intensityArray = spec.intensities()
 	
-	#spec.integration_time_microsec(intTime)
-
-#write the wavelengths captured by the spectrometer to the opened file	
-#f.write(spec.wavelengths())
-#print spec.wavelengths()
-
-try:
-	while True:
-		wavelengthArray = spec.wavelengths()
-		intensityArray = spec.intensities()
-
-		f.write(str(st))
-		f.write('\n')
-
-		for x in range(0, len(wavelengthArray)):
+	f.write(str(st))
+	f.write('\n')
+	
+	for x in range(0, len(wavelengthArray)):
 			f.write(str(wavelengthArray[x]))
 			f.write(',')
 			f.write(str(intensityArray[x]))
 			f.write('\n')
-		print("Data collected")
-		time.sleep(3)	
-except KeyboardInterrupt:
-	print("We closed")
-	f.close()
-
-
-# while True:
-    # var = input("Start Data collecting with an input of 2: ")
-    # if not var:
-        # continue
-
-    # try:
-        # while var == 2: 
-			# # wavelengthArray = spec.wavelengths()
-			# # intensityArray = spec.intensities()
-
-			# # f.write(str(st))
-			# # f.write('\n')
-
-			# # for x in range(0, len(wavelengthArray)):
-				# # f.write(str(wavelengthArray[x]))
-				# # f.write(',')
-				# # f.write(str(intensityArray[x]))
-				# # f.write('\n')
-	
-			# # time.sleep(3)		
-	# except KeyboardInterrupt:
-		# f.close()
+	print("Data collected")
         
--- pushButtonStart.py
+++ pushButtonStart.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-#import USB2000Acquire
+import USB2000Acquire as ocean
 
 GPIO.setmode(GPIO.BCM)
 
@@ -9,5 +9,6 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
-        print('Button Pressed')
-        time.sleep(0.2)
+		ocean.capture_spectrum()
+		print('Button Pressed')		
+		time.sleep(0.2)
